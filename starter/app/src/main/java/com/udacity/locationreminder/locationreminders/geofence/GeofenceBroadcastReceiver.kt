2ec11c1637bc096260fa7e9106c083f5ec83e173
Sergio Aragones <sergioat7@gmail.com>
2021-07-22 13:46:22 +0200

    Created notification when user enters in geofence area

--- starter/app/src/main/java/com/udacity/locationreminder/locationreminders/geofence/GeofenceBroadcastReceiver.kt
+++ starter/app/src/main/java/com/udacity/locationreminder/locationreminders/geofence/GeofenceBroadcastReceiver.kt
@@ -3,10 +3,6 @@ package com.udacity.locationreminder.locationreminders.geofence
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.util.Log
-import com.google.android.gms.location.Geofence
-import com.google.android.gms.location.GeofenceStatusCodes
-import com.google.android.gms.location.GeofencingEvent
 import com.udacity.locationreminder.locationreminders.savereminder.SaveReminderFragment
 
 /**
@@ -22,22 +18,9 @@ import com.udacity.locationreminder.locationreminders.savereminder.SaveReminderF
 class GeofenceBroadcastReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
+
         if (intent.action == SaveReminderFragment.ACTION_GEOFENCE_EVENT) {
-
-            val geofencingEvent = GeofencingEvent.fromIntent(intent)
-            if (geofencingEvent.hasError()) {
-                val errorMessage =
-                    GeofenceStatusCodes.getStatusCodeString(geofencingEvent.errorCode)
-                Log.e(this.javaClass.name, errorMessage)
-                return
-            }
-
-            if (geofencingEvent.geofenceTransition == Geofence.GEOFENCE_TRANSITION_ENTER) {
-                Log.i(this.javaClass.name, "Geofence entered")
-
-                val triggeringGeofences = geofencingEvent.triggeringGeofences
-                //TODO: create notification for triggeringGeofences
-            }
+            GeofenceTransitionsJobIntentService.enqueueWork(context, intent)
         }
     }
 }
