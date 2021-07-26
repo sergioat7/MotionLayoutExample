dfdf42295693a2989c2312994649a6eaaf4de9c1
Sergio Aragonés <sergioat7@gmail.com>
2021-07-27 00:06:39 +0200

    Code refactoring

--- starter/app/src/main/java/com/udacity/locationreminder/authentication/AuthenticationActivity.kt
+++ starter/app/src/main/java/com/udacity/locationreminder/authentication/AuthenticationActivity.kt
@@ -36,9 +36,6 @@ class AuthenticationActivity : AppCompatActivity() {
         binding.buttonLogin.setOnClickListener {
             launchSignInFlow()
         }
-
-//          TODO: a bonus is to customize the sign in flow to look nice using :
-        //https://github.com/firebase/FirebaseUI-Android/blob/master/auth/README.md#custom-layout
     }
 
     override fun onResume() {
