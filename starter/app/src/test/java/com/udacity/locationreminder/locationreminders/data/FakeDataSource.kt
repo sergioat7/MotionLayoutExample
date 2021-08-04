8c9e2713cf56b2d9d0b7cdff929c2e869da6df75
Sergio Aragonés <sergioat7@gmail.com>
2021-08-04 23:38:25 +0200

    Code refactoring

--- starter/app/src/test/java/com/udacity/locationreminder/locationreminders/data/FakeDataSource.kt
+++ starter/app/src/test/java/com/udacity/locationreminder/locationreminders/data/FakeDataSource.kt
@@ -24,5 +24,9 @@ class FakeDataSource : ReminderDataSource {
         TODO("delete all the reminders")
     }
 
+    override suspend fun deleteReminder(reminderId: String) {
+        TODO("delete the reminder")
+    }
+
 
 }
