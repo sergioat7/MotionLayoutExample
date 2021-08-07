52d85412b43c7a8ce36112b2f9f6356dc00a7766
Sergio Aragonés <sergioat7@gmail.com>
2021-08-07 19:50:40 +0200

    Created end to end tests for RemindersActivity

--- starter/app/src/androidTest/java/com/udacity/locationreminder/RemindersActivityTest.kt
+++ starter/app/src/androidTest/java/com/udacity/locationreminder/RemindersActivityTest.kt
@@ -1,18 +1,39 @@
 package com.udacity.locationreminder
 
 import android.app.Application
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider.getApplicationContext
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.IdlingRegistry
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
+import com.google.firebase.auth.FirebaseAuth
+import com.udacity.locationreminder.locationreminders.RemindersActivity
 import com.udacity.locationreminder.locationreminders.data.ReminderDataSource
+import com.udacity.locationreminder.locationreminders.data.dto.ReminderDTO
 import com.udacity.locationreminder.locationreminders.data.local.LocalDB
 import com.udacity.locationreminder.locationreminders.data.local.RemindersLocalRepository
+import com.udacity.locationreminder.locationreminders.reminderdescription.ReminderDescriptionViewModel
 import com.udacity.locationreminder.locationreminders.reminderslist.RemindersListViewModel
 import com.udacity.locationreminder.locationreminders.savereminder.SaveReminderViewModel
+import com.udacity.locationreminder.util.DataBindingIdlingResource
+import com.udacity.locationreminder.util.monitorActivity
+import com.udacity.locationreminder.utils.EspressoIdlingResource
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
+import org.junit.After
 import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
+import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.context.loadKoinModules
 import org.koin.core.context.startKoin
 import org.koin.core.context.stopKoin
 import org.koin.dsl.module
@@ -28,6 +49,12 @@ class RemindersActivityTest :
     private lateinit var repository: ReminderDataSource
     private lateinit var appContext: Application
 
+    // An Idling Resource that waits for Data Binding to have no pending bindings
+    private val dataBindingIdlingResource = DataBindingIdlingResource()
+
+    @get:Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+
     /**
      * As we use Koin as a Service Locator Library to develop our code, we'll also use Koin to test our code.
      * at this step we will initialize Koin related code to be able to use it in out testing.
@@ -36,9 +63,15 @@ class RemindersActivityTest :
     fun init() {
         stopKoin()//stop the original app koin
         appContext = getApplicationContext()
-        val myModule = module {
+        val testModules = module {
             viewModel {
                 RemindersListViewModel(
+                    appContext,
+                    get() as ReminderDataSource
+                )
+            }
+            viewModel {
+                ReminderDescriptionViewModel(
                     appContext,
                     get() as ReminderDataSource
                 )
@@ -49,12 +82,13 @@ class RemindersActivityTest :
                     get() as ReminderDataSource
                 )
             }
-            single { RemindersLocalRepository(get()) as ReminderDataSource }
+            single { RemindersLocalRepository(get()) as ReminderDataSource }//DO NOT remove the cast
             single { LocalDB.createRemindersDao(appContext) }
         }
         //declare a new koin module
         startKoin {
-            modules(listOf(myModule))
+            androidContext(getApplicationContext())
+            loadKoinModules(testModules)
         }
         //Get our real repository
         repository = get()
@@ -65,7 +99,105 @@ class RemindersActivityTest :
         }
     }
 
+    @Before
+    fun setup() {
+        IdlingRegistry.getInstance().register(dataBindingIdlingResource)
+        IdlingRegistry.getInstance().register(EspressoIdlingResource.countingIdlingResource)
 
-//    TODO: add End to End testing to the app
+        runBlocking {
+            FirebaseAuth.getInstance().signOut()
+            FirebaseAuth.getInstance().signInWithEmailAndPassword("test@test.com", "Test123")
+        }
+    }
 
+    @After
+    fun unregisterIdlingResources() {
+        IdlingRegistry.getInstance().unregister(dataBindingIdlingResource)
+        IdlingRegistry.getInstance().unregister(EspressoIdlingResource.countingIdlingResource)
+    }
+
+    @Test
+    fun addReminder() {
+        // Start up Reminders screen
+        val activityScenario = ActivityScenario.launch(RemindersActivity::class.java)
+        dataBindingIdlingResource.monitorActivity(activityScenario)
+
+        // Open save reminder view and set a title
+        onView(withId(R.id.addReminderFAB)).perform(click())
+        onView(withId(R.id.reminderTitle)).perform(typeText("Title"), closeSoftKeyboard())
+
+        // Open select location view and create a marker
+        onView(withId(R.id.selectLocation)).perform(click())
+        onView(withId(R.id.map)).check(matches(isDisplayed()))
+        onView(withContentDescription("My Location")).perform(click())
+        onView(withId(R.id.button_save)).perform(click())
+        onView(withId(com.google.android.material.R.id.snackbar_text)).check(matches(withText("Please select location")))
+        runBlocking { delay(3000) }
+        onView(withId(R.id.map)).perform(longClick())
+        onView(withId(R.id.button_save)).perform(click())
+
+        // Save reminder and verify has been created
+        onView(withId(R.id.saveReminder)).perform(click())
+        onView(withText("Title")).check(matches(isDisplayed()))
+
+        // Make sure the activity is closed before resetting the db
+        activityScenario.close()
+    }
+
+    @Test
+    fun seeReminder() = runBlocking {
+        val reminder = ReminderDTO("Title", "Description", "Location", 37.0, -122.0)
+        repository.saveReminder(reminder)
+
+        // Start up Reminders screen
+        val activityScenario = ActivityScenario.launch(RemindersActivity::class.java)
+        dataBindingIdlingResource.monitorActivity(activityScenario)
+
+        // Click on the reminder on the list and verify that all the data is correct
+        onView(withText("Title")).perform(click())
+        onView(withId(R.id.tvTitle)).check(matches(withText("Title")))
+        onView(withId(R.id.tvDescription)).check(matches(withText("Description")))
+        onView(withId(R.id.tvLocation)).check(matches(withText("Location")))
+        onView(withId(R.id.tvLatLng)).check(matches(withText("Lat: 37.00000, Long: -122.00000")))
+
+        // Make sure the activity is closed before resetting the db
+        activityScenario.close()
+    }
+
+    @Test
+    fun addReminder_seeReminder_deleteReminder() {
+        // Start up Reminders screen
+        val activityScenario = ActivityScenario.launch(RemindersActivity::class.java)
+        dataBindingIdlingResource.monitorActivity(activityScenario)
+
+        // Open save reminder view and set a title
+        onView(withId(R.id.addReminderFAB)).perform(click())
+        onView(withId(R.id.reminderTitle)).perform(typeText("Title"), closeSoftKeyboard())
+
+        // Open select location view and create a marker
+        onView(withId(R.id.selectLocation)).perform(click())
+        onView(withId(R.id.map)).check(matches(isDisplayed()))
+        onView(withContentDescription("My Location")).perform(click())
+        onView(withId(R.id.button_save)).perform(click())
+        onView(withId(com.google.android.material.R.id.snackbar_text)).check(matches(withText("Please select location")))
+        runBlocking { delay(3000) }
+        onView(withId(R.id.map)).perform(longClick())
+        onView(withId(R.id.button_save)).perform(click())
+
+        // Save reminder and verify has been created
+        onView(withId(R.id.saveReminder)).perform(click())
+        onView(withText("Title")).check(matches(isDisplayed()))
+
+        // Click on the new reminder on the list and verify that all the data is correct
+        onView(withText("Title")).perform(click())
+        onView(withId(R.id.tvTitle)).check(matches(withText("Title")))
+        onView(withId(R.id.tvLocation)).check(matches(withText("Custom marker")))
+
+        // Delete reminder and verify has been deleted
+        onView(withId(R.id.button_delete)).perform(click())
+        onView(withText("Title")).check(doesNotExist())
+
+        // Make sure the activity is closed before resetting the db
+        activityScenario.close()
+    }
 }
--- starter/app/src/main/java/com/udacity/locationreminder/locationreminders/data/local/RemindersLocalRepository.kt
+++ starter/app/src/main/java/com/udacity/locationreminder/locationreminders/data/local/RemindersLocalRepository.kt
@@ -3,6 +3,7 @@ package com.udacity.locationreminder.locationreminders.data.local
 import com.udacity.locationreminder.locationreminders.data.ReminderDataSource
 import com.udacity.locationreminder.locationreminders.data.dto.ReminderDTO
 import com.udacity.locationreminder.locationreminders.data.dto.Result
+import com.udacity.locationreminder.utils.wrapEspressoIdlingResource
 import kotlinx.coroutines.*
 
 /**
@@ -23,10 +24,12 @@ class RemindersLocalRepository(
      * @return Result the holds a Success with all the reminders or an Error object with the error message
      */
     override suspend fun getReminders(): Result<List<ReminderDTO>> = withContext(ioDispatcher) {
-        return@withContext try {
-            Result.Success(remindersDao.getReminders())
-        } catch (ex: Exception) {
-            Result.Error(ex.localizedMessage)
+        wrapEspressoIdlingResource {
+            return@withContext try {
+                Result.Success(remindersDao.getReminders())
+            } catch (ex: Exception) {
+                Result.Error(ex.localizedMessage)
+            }
         }
     }
 
@@ -35,8 +38,10 @@ class RemindersLocalRepository(
      * @param reminder the reminder to be inserted
      */
     override suspend fun saveReminder(reminder: ReminderDTO) =
-        withContext(ioDispatcher) {
-            remindersDao.saveReminder(reminder)
+        wrapEspressoIdlingResource {
+            withContext(ioDispatcher) {
+                remindersDao.saveReminder(reminder)
+            }
         }
 
     /**
@@ -45,15 +50,17 @@ class RemindersLocalRepository(
      * @return Result the holds a Success object with the Reminder or an Error object with the error message
      */
     override suspend fun getReminder(id: String): Result<ReminderDTO> = withContext(ioDispatcher) {
-        try {
-            val reminder = remindersDao.getReminderById(id)
-            if (reminder != null) {
-                return@withContext Result.Success(reminder)
-            } else {
-                return@withContext Result.Error("Reminder not found!")
+        wrapEspressoIdlingResource {
+            try {
+                val reminder = remindersDao.getReminderById(id)
+                if (reminder != null) {
+                    return@withContext Result.Success(reminder)
+                } else {
+                    return@withContext Result.Error("Reminder not found!")
+                }
+            } catch (e: Exception) {
+                return@withContext Result.Error(e.localizedMessage)
             }
-        } catch (e: Exception) {
-            return@withContext Result.Error(e.localizedMessage)
         }
     }
 
@@ -61,8 +68,10 @@ class RemindersLocalRepository(
      * Deletes all the reminders in the db
      */
     override suspend fun deleteAllReminders() {
-        withContext(ioDispatcher) {
-            remindersDao.deleteAllReminders()
+        wrapEspressoIdlingResource {
+            withContext(ioDispatcher) {
+                remindersDao.deleteAllReminders()
+            }
         }
     }
 
@@ -70,9 +79,11 @@ class RemindersLocalRepository(
      * Deletes a specific reminder in the db
      */
     override suspend fun deleteReminder(reminderId: String) {
-        withContext(ioDispatcher) {
-            val reminder = remindersDao.getReminderById(reminderId)
-            remindersDao.deleteReminder(reminder)
+        wrapEspressoIdlingResource {
+            withContext(ioDispatcher) {
+                val reminder = remindersDao.getReminderById(reminderId)
+                remindersDao.deleteReminder(reminder)
+            }
         }
     }
 }
